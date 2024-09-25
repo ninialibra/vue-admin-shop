760f58f107e528e81fb5bee74fd074aad6913a8f
Tania <tanialibramg@gmail.com>
2024-09-25 22:03:30 +0200

    inicio

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- component -->
+
+  <TopMenu></TopMenu>
+
+  <router-view></router-view>
+
+  <!-- Footer -->
+  <CustomFooter></CustomFooter>
+</template>
+
+<script lang="ts" setup>
+import CustomFooter from '../components/CustomFooter.vue';
+import TopMenu from '../components/TopMenu.vue';
+</script>
